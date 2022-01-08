e88e0d2712f23e1fc575d64d98f62b40dfe9c53a
JonnyRoller23 <maxim55miui11@gmail.com>
2022-01-09 00:00:50 +0300

    sdm439-common: Added support for long screenshot

--- sdm439.mk
+++ sdm439.mk
@@ -101,6 +101,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
 
+# LongShot
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/com.asus.stitchimage.service-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/com.asus.stitchimage.service-hiddenapi-package-whitelist.xml \
+    $(LOCAL_PATH)/configs/com.asus.stitchimage-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/com.asus.stitchimage-hiddenapi-package-whitelist.xml      
+
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
@@ -112,12 +117,21 @@ PRODUCT_PACKAGES += \
     libstdc++.vendor \
     Snap
 
-PRODUCT_PACKAGES += \
-    libwa_dof
-
 # Dirac
 PRODUCT_PACKAGES += \
     Dirac
+
+PRODUCT_PACKAGES += \
+    libwa_dof
+
+PRODUCT_PACKAGES += \
+    CustomQSLonghsot    
+
+PRODUCT_PACKAGES += \
+    StitchImage
+
+PRODUCT_PACKAGES += \
+    StitchImageService
 
 # Display
 PRODUCT_PACKAGES += \
@@ -346,7 +360,11 @@ PRODUCT_PACKAGES += \
 
 # Whitelisted app
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/permissions/privapp-permissions-huaqin.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-huaqin.xml
+    $(LOCAL_PATH)/permissions/privapp-permissions-huaqin.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-huaqin.xml \
+    $(LOCAL_PATH)/permissions/privapp-permissions-com.asus.stitchimage.service.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.asus.stitchimage.service.xml \
+    $(LOCAL_PATH)/permissions/privapp-permissions-com.asus.stitchimage.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.asus.stitchimage.xml \
+    $(LOCAL_PATH)/permissions/privapp-permissions-com.customqs.lonshot.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.customqs.lonshot.xml \
+    $(LOCAL_PATH)/default-permissions/default-asus-permissions.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/default-permissions/default-asus-permissions.xml
 
 # Wifi
 PRODUCT_PACKAGES += \
